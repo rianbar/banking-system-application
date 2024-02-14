a1cc52371fddbb22d6f464939a4dfaada652f23d
rian <rianbarroso596@gmail.com>
2024-02-14 14:15:50 -0300

    integration test patially implemented

--- register-system/src/test/java/com/bank/registersystem/controller/UserControllerTest.java
+++ register-system/src/test/java/com/bank/registersystem/controller/UserControllerTest.java
@@ -8,8 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -18,39 +17,46 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UserControllerTest {
 
     private String token;
-
+    private final Long id = 1L;
+    private final Long unavailableId = 500L;
+    private String jsonRequest;
     @Autowired
     private MockMvc mvc;
 
     @BeforeEach
     void setUp() {
-        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoLWFwcGxpY2F0aW9uIiwic3ViIjoibWFyaWFuYXJhYnVkYWdv" +
-                "c3Rvc2FAZW1haWwuY29tIiwiZXhwIjoxNzA3ODUwNDMzfQ.L9NRzL1X8btu0YsNS97Q0Jmdzali4fxXCuXj334hKlI";
+        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoLWFwcGxpY2F0aW9uIiwic3ViIjoiYW55MTJAZ" +
+                "W1haWwuY29tIiwiZXhwIjoxNzA3OTM3MDc2fQ.XMmW_bCcJotPjYU9PIW0QASx6zkDF9c4PkOB3qs5fMQ";
+
+        String email = "anyone@email.com";
+        String identity = "122.323.555-13";
+        jsonRequest = "{\"name\":\"any\",\"identity\":\"" + identity + "\"," +
+                "\"email\":\"" + email + "\",\"password\":\"any\",\"userType\":\"USER\"}";
     }
 
     @Test
-    void getUserById_withValidData_returnsObject() throws Exception {
-        mvc.perform(get("/user/{id}", 3)
-                        .header("authorization", "Bearer " + token))
-                .andExpect(status().isOk())
+    void createUser_withValidData_returnsObject() throws Exception {
+        mvc.perform(post("/user/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
-    void getUserById_withInvalidData_throwsException() throws Exception {
-        String expectedJson = "{\"code\":404,\"type\":\"404 NOT_FOUND\",\"message\":\"user 'id' not found!\"}";
-
-        mvc.perform(get("/user/{id}", 500)
-                .header("authorization", "Bearer " + token))
-                .andExpect(status().isNotFound())
-                .andExpect(content().json(expectedJson));
+    void createUser_withInvalidData_throwException() throws Exception {
+        mvc.perform(post("/user/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isForbidden())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
     void loginUser_withValidUser_returnToken() throws Exception {
         mvc.perform(post("/user/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"email\": \"rianbarroso1@email.com\",\"password\": \"riansantos\"}"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"email\": \"rianbarroso1@email.com\",\"password\": \"riansantos\"}"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_PLAIN + ";charset=UTF-8"));
     }
@@ -60,9 +66,53 @@ class UserControllerTest {
         String expectedJson = "{\"code\":401,\"type\":\"401 UNAUTHORIZED\",\"message\":\"email or password is wrong!\"}";
 
         mvc.perform(post("/user/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"email\": \"any@email.com\",\"password\": \"any\"}"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"email\": \"any@email.com\",\"password\": \"any\"}"))
                 .andExpect(status().isUnauthorized())
                 .andExpect(content().json(expectedJson));
     }
+
+    @Test
+    void getUserById_withValidData_returnsObject() throws Exception {
+        mvc.perform(get("/user/{id}", id)
+                        .header("authorization", "Bearer " + token))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void getUserById_withInvalidData_throwsException() throws Exception {
+        String expectedJson = "{\"code\":404,\"type\":\"404 NOT_FOUND\",\"message\":\"user 'id' not found!\"}";
+
+        mvc.perform(get("/user/{id}", unavailableId)
+                .header("authorization", "Bearer " + token))
+                .andExpect(status().isNotFound())
+                .andExpect(content().json(expectedJson));
+    }
+
+
+
+    @Test
+    void updateUser_withValidData_returnsObject() throws Exception {
+        mvc.perform(put("/user/update/{id}", id)
+                .header("authorization", "Bearer " + token)
+                .contentType(MediaType.APPLICATION_JSON).content(jsonRequest))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void deleteUserTest() throws Exception {
+        mvc.perform(delete("/user/delete/{id}", id)
+                        .header("authorization", "Bearer " + token))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deleteUserTest_withInvalidData() throws Exception {
+        mvc.perform(delete("/user/delete/{id}", unavailableId)
+                        .header("authorization", "Bearer " + token))
+                .andExpect(status().isNotFound());
+    }
+
 }
